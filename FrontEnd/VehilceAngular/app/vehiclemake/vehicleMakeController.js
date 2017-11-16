63a1dc7216270e9e87d993a4bb64532f48102dbd
Miodrag Matijaš <miodrag.matijas@gmail.com>
2017-11-16 15:22:17 +0100

    Novo

---
+++ FrontEnd/VehilceAngular/app/vehiclemake/vehicleMakeController.js
@@ -0,0 +1,13 @@
+﻿app.controller('VehicleMakeController', function ($scope, VehicleMakeService) {
+    getAll();
+
+    function getAll() {
+        var serviceCall = VehicleMakeService.getSubs();
+        serviceCall.then(function (d) {
+            $scope.vehicleMake = d.data;
+        }, function (error) {
+                $log.error('Fetch error');
+            }
+        );
+    }
+});
---
+++ FrontEnd/VehilceAngular/app/vehiclemake/vehicleMakeService.js
@@ -0,0 +1,6 @@
+﻿app.service("VehicleMakeService", function ($http) {
+    this.getSubs = function () {
+        return $http.get('http://localhost:64407/api/vehicle');
+    };
+});
+
