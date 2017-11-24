19f7a3d96b17819c17b18d2e1f28a8021f03d471
Miodrag Matijaš <miodrag.matijas@gmail.com>
2017-11-24 18:57:54 +0100

    Delete

--- FrontEnd/VehilceAngular/app/vehiclemake/makeDetails.js
+++ FrontEnd/VehilceAngular/app/vehiclemake/makeDetails.js
@@ -1,4 +1,4 @@
-﻿vehicleModule.controller('MakeDetailsController', function ($scope,$location,$http,$routeParams) {    
+﻿vehicleModule.controller('MakeDetailsController', function ($scope, $location, $http, $routeParams, $window) {
 
     $http.get('http://localhost:64407/api/vehicle/' + $routeParams.ID)
 
@@ -6,9 +6,20 @@
             $scope.ID = response.data.ID;
             $scope.Name = response.data.Name;
             $scope.Abrv = response.data.Abrv;
-        });    
-        
+        });
+
+    $scope.makeDelete =  function (xx) {
+
+        $http.delete('http://localhost:64407/api/vehicle/' + $routeParams.ID)
+
+            .then(function () {
+                msg = "Item " + xx + " is Deleted!!!";
+                $window.alert(msg);
+                $location.path("vehicleMake");
+            });        
+    };
+
 });
-   
+
 
 
--- FrontEnd/VehilceAngular/app/vehiclemake/vehicleMakeController.js
+++ FrontEnd/VehilceAngular/app/vehiclemake/vehicleMakeController.js
@@ -1,4 +1,4 @@
-﻿vehicleModule.controller('VehicleMakeController', function ($scope, $http,$location) {
+﻿vehicleModule.controller('VehicleMakeController', function ($scope, $http) {
 
     $http.get('http://localhost:64407/api/vehicle')
         .then(function (response) { $scope.vehicleMake = response.data; });
