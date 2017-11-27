1618bab7fec919ed6257cb48e9c3e758f0716ef9
Miodrag Matijaš <miodrag.matijas@gmail.com>
2017-11-27 14:12:45 +0100

    novo

--- FrontEnd/VehilceAngular/app/vehiclemake/makeEdit.js
+++ FrontEnd/VehilceAngular/app/vehiclemake/makeEdit.js
@@ -1,4 +1,4 @@
-﻿vehicleModule.controller('MakeEditController', function ($scope, $routeParams, $http) {
+﻿vehicleModule.controller('MakeEditController', function ($scope, $routeParams, $http, $window) {
 
     $http.get('http://localhost:64407/api/vehicle/' + $routeParams.ID)
 
@@ -6,6 +6,23 @@
             $scope.ID = response.data.ID;
             $scope.Name = response.data.Name;
             $scope.Abrv = response.data.Abrv;
+
+
+            $scope.update = function () {
+                var obj = {
+                    ID: response.data.ID,
+                    Name: response.data.Name,
+                    Abrv: response.data.Abrv
+                };
+
+                $http.put('http://localhost:64407/api/vehicle/', obj).success(function (data) {
+
+                    $windwow.alert("Success Updated!!!");
+
+                }).error(function (data) {
+                    $window.alert("Error! " + data.Message);
+                });
+            };
 
         });
 
